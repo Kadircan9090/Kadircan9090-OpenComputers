73ea7b88027e4bc39172dac93bbc0a43fc70f457
Kadircan Şahin <131601947+Kadircan9090@users.noreply.github.com>
2024-09-25 23:55:51 +0300

    Update Text.lua

--- Lua/Text.lua
+++ Lua/Text.lua
@@ -1,1 +1,28 @@
+local event = require("event")
+local component = require("component")
+local internet = component.internet
+local cardwriter = component.os_cardwriter
+local keypad = component.os_keypad
 
+local args = {...}
+local pin = ""
+
+do
+    keypad.setDisplay("Your PIN")
+    while true do
+        local _, _, _, char = event.pull("keypad")
+        if char == "#" then
+            keypad.setDisplay("Ok")
+            os.sleep(1)
+            keypad.setDisplay("")
+            break
+        elseif char == "*" then
+            keypad.setDisplay("Resetted")
+            pin = ""
+            os.sleep(1)
+            keypad.setDisplay("Your PIN")
+        else
+            pin = pin .. char
+        end
+    end
+end

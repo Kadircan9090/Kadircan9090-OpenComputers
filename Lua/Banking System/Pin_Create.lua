83199c9c61ad6e890a72127071fdcbb4c9b81cad
Kadircan Şahin <131601947+Kadircan9090@users.noreply.github.com>
2025-06-02 13:41:12 +0300

    Update Pin_Create.lua

--- Lua/Banking System/Pin_Create.lua
+++ Lua/Banking System/Pin_Create.lua
@@ -12,10 +12,16 @@ do
     while true do
         local _, _, _, char = event.pull("keypad")
         if char == "#" then
-            keypad.setDisplay("Ok")
-            os.sleep(1)
-            keypad.setDisplay("")
-            break
+            if pin == "" then
+                keypad.setDisplay("Empty PIN")
+                os.sleep(1)
+                keypad.setDisplay("Your PIN")
+            else
+                keypad.setDisplay("Ok")
+                os.sleep(1)
+                keypad.setDisplay("")
+                break
+            end
         elseif char == "*" then
             keypad.setDisplay("Resetted")
             pin = ""
@@ -27,18 +33,22 @@ do
     end
 end
 
-local request = "username=" .. (args[1] or "") .. "&pin=" .. (pin or "")
+local request = "username=" .. (args[1] or "") .. "&pin=" .. pin
+local headers = {["Content-Type"] = "application/x-www-form-urlencoded"}
 
-local success, response = pcall(internet.request, "http://localhost/create", request)
+local success, response = pcall(internet.request, "http://localhost/create", "POST", request, headers)
 
 if success then
     local id = ""
     for chunk in response do
         id = id .. chunk
     end
-
     response.close()
-    cardwriter.write(id, "Bankcard", false)
+
+    local wrote = cardwriter.write(id, "Bankcard", false)
+    if not wrote then
+        print("Failed to write to card")
+    end
 else
     print("Error occurred: " .. response)
 end
